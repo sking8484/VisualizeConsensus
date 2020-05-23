dc4a39070d38d29076d1311c361a500a34c7fa07
Darst King <sking8484@gmail.com>
2020-05-23 00:52:23 -0700

    Created the skeleton for parseData rectangle class

--- parseData.py
+++ parseData.py
@@ -1,2 +1,11 @@
-#The first code written
+# Rectangle class 
+
+class Rectangle:
+	def __init__(self,lt,lb,rt,rb):
+		self.lt = lt #(x,y)
+		self.lb = lb #(x,y)
+		self.rt = rt #(x,y)
+		self.rb = rb #(x,y)
+	def calculateArea(self):
+		#write a function to calculate the area 
 
